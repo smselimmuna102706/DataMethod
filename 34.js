dac6516dd53744ef37623283b479e8d1e5e1efeb
smselimmuna102706 <111196494+smselimmuna102706@users.noreply.github.com>
2022-09-12 21:11:51 +0600

    Add files via upload

---
+++ 34.js
@@ -0,0 +1,26 @@
+
+var  date = new Date();
+console.log(date);
+
+var Fullyear =date.getFullYear();
+console.log(Fullyear);
+
+var Month = date.getMonth();
+console.log(Month);
+
+var day = date.getDay();
+console.log(day);
+
+var hourse = date.getHours();
+console.log(hourse);
+
+var miniute = date.getMinutes();
+console.log(miniute);
+
+var seconds = date.getSeconds();
+console.log(seconds);
+
+
+var miliseconds = date.getMilliseconds();
+console.log(miliseconds);
+
